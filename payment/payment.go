f3b616f0e7dd99a59b283e6cd75bdf93ef4700ed
Alex <heinritzi.alex@gmail.com>
2020-06-17 12:59:21 +0200

    payment fixed

--- payment/payment.go
+++ payment/payment.go
@@ -2,6 +2,7 @@ package payment
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/micro/go-micro/v2"
 	"github.com/micro/go-micro/v2/logger"
@@ -17,13 +18,15 @@ func New(publisher micro.Event) *Payment {
 }
 
 func (p *Payment) ReceivePayment(ctx context.Context, req *api.PaymentRequest, res *api.PaymentResponse) error {
-	//msg := fmt.Sprintf("Payment request for", req.OrderID)
+	msg := fmt.Sprintf("Payment request for", req.OrderID)
 
-	//logger.info(msg)
+	logger.Info(msg)
 
 	err := "hallo"
 
-	if err != "hallo" {
+	x := p.publisher.Publish(context.Background(), &api.PaymentEvent{OrderID: req.OrderID})
+
+	if x != nil {
 		logger.Errorf("error while publishing %v", err)
 	}
 
